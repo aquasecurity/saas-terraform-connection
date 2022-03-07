96eff8744aea4b524f1775b8ffd7ae11ab101d92
tomerdanielaqua <59365897+tomerdanielaqua@users.noreply.github.com>
2022-03-07 13:55:28 +0200

    Version 1 (#17)
    * Initial commit TF version 1 supprt
    
    * added supplement role
    
    Co-authored-by: Tomer Shafir <tomer.shafir@aquasec.com>

--- iam_cloudsploit_role_gov.tf
+++ iam_cloudsploit_role_gov.tf
@@ -1,34 +1,36 @@
+data "aws_iam_policy_document" "cloudsploit_cross_account_role_document-gov" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws-us-gov:iam::099193770462:root"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "sts:ExternalId"
+
+      values = [
+        "${var.cloudsploit_external_id}"
+      ]
+    }
+  }
+}
+
 resource "aws_iam_role" "cloudsploit_cross_account_role-gov" {
   name = "tf-cloudsploit"
-
-  # enable this if use_aws_gov == true
+  # disable this if use_aws_gov == true
   count = "${var.use_aws_gov ? 1 : 0}"
+  assume_role_policy = data.aws_iam_policy_document.cloudsploit_cross_account_role_document-gov
+}
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": "arn:aws-us-gov:iam::099193770462:root"
-      },
-      "Action": "sts:AssumeRole",
-      "Condition": {
-        "StringEquals": {
-          "sts:ExternalId": "${var.cloudsploit_external_id}"
-        }
-      }
-    }
-  ]
-}
-EOF
-}
 
 resource "aws_iam_role_policy_attachment" "cloudsploit_cross_account_attach-gov" {
-  # enable this if use_aws_gov == true
+  # disable this if use_aws_gov == true
   count = "${var.use_aws_gov ? 1 : 0}"
 
-  role       = "${aws_iam_role.cloudsploit_cross_account_role-gov.name}"
+  role       = aws_iam_role.cloudsploit_cross_account_role-gov[*].name
   policy_arn = "arn:aws:iam::aws:policy/SecurityAudit"
 }
+
