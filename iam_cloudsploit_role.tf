a8045117be8572c62467fe9072810287a2695cdf
tomgun11 <31482512+tomgun11@users.noreply.github.com>
2022-03-15 22:03:49 +0200

    Support version 1 (#18)
    Co-authored-by: Tomer Shafir <tomer.shafir@aquasec.com>

--- iam_cloudsploit_role.tf
+++ iam_cloudsploit_role.tf
@@ -1,35 +1,34 @@
-data "aws_iam_policy_document" "cloudsploit_cross_account_role_document" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "AWS"
-      identifiers = ["arn:aws:iam::057012691312:root"]
-    }
-
-    condition {
-      test     = "StringEquals"
-      variable = "sts:ExternalId"
-
-      values = [
-        "${var.cloudsploit_external_id}"
-      ]
-    }
-  }
-}
-
 resource "aws_iam_role" "cloudsploit_cross_account_role" {
   name = "tf-cloudsploit"
-  # disable this if use_aws_gov == true
-  count = var.use_aws_gov ? 1 : 0
-  assume_role_policy = data.aws_iam_policy_document.cloudsploit_cross_account_role_document
-}
 
+  # disable this if use_aws_gov == true
+  count = var.use_aws_gov ? 0 : 1
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::057012691312:root"
+      },
+      "Action": "sts:AssumeRole",
+      "Condition": {
+        "StringEquals": {
+          "sts:ExternalId": "${var.cloudsploit_external_id}"
+        }
+      }
+    }
+  ]
+}
+EOF
+}
 
 resource "aws_iam_role_policy_attachment" "cloudsploit_cross_account_attach" {
   # disable this if use_aws_gov == true
-  count = var.use_aws_gov ? 1 : 0
+  count = var.use_aws_gov ? 0 : 1
 
-  role       = aws_iam_role.cloudsploit_cross_account_role[*].name
+  role       = aws_iam_role.cloudsploit_cross_account_role[count.index].name
   policy_arn = "arn:aws:iam::aws:policy/SecurityAudit"
 }
--- iam_cloudsploit_role_gov.tf
+++ iam_cloudsploit_role_gov.tf
@@ -1,36 +1,34 @@
-data "aws_iam_policy_document" "cloudsploit_cross_account_role_document-gov" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "AWS"
-      identifiers = ["arn:aws-us-gov:iam::099193770462:root"]
-    }
-
-    condition {
-      test     = "StringEquals"
-      variable = "sts:ExternalId"
-
-      values = [
-        "${var.cloudsploit_external_id}"
-      ]
-    }
-  }
-}
-
 resource "aws_iam_role" "cloudsploit_cross_account_role-gov" {
   name = "tf-cloudsploit"
-  # disable this if use_aws_gov == true
-  count = "${var.use_aws_gov ? 1 : 0}"
-  assume_role_policy = data.aws_iam_policy_document.cloudsploit_cross_account_role_document-gov
-}
 
+  # enable this if use_aws_gov == true
+  count = var.use_aws_gov ? 1 : 0
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws-us-gov:iam::099193770462:root"
+      },
+      "Action": "sts:AssumeRole",
+      "Condition": {
+        "StringEquals": {
+          "sts:ExternalId": "${var.cloudsploit_external_id}"
+        }
+      }
+    }
+  ]
+}
+EOF
+}
 
 resource "aws_iam_role_policy_attachment" "cloudsploit_cross_account_attach-gov" {
-  # disable this if use_aws_gov == true
-  count = "${var.use_aws_gov ? 1 : 0}"
+  # enable this if use_aws_gov == true
+  count = var.use_aws_gov ? 1 : 0
 
-  role       = aws_iam_role.cloudsploit_cross_account_role-gov[*].name
+  role       = aws_iam_role.cloudsploit_cross_account_role-gov[count.index].name
   policy_arn = "arn:aws:iam::aws:policy/SecurityAudit"
 }
-
