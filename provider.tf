847553d3eb8822116292756766f86743d2119598
Tomer Shafir <tomer.shafir@aquasec.com>
2022-07-11 19:51:08 +0300

    SAAS-6921 dynamic region

--- provider.tf
+++ provider.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.5.0"
+      version = "~> 4.2.0"
     }
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
 }
