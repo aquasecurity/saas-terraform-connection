d4fea971cb6d10bb3b112a562dd24b495600483b
tomgun11 <31482512+tomgun11@users.noreply.github.com>
2022-07-12 13:26:12 +0300

    Ts/version requirement (#23)
    * SAAS-6921 dynamic region
    
    * Added newline
    
    Co-authored-by: Tomer Shafir <tomer.shafir@aquasec.com>

--- variable.tf
+++ variable.tf
@@ -6,3 +6,8 @@ variable "use_aws_gov" {
   default     = false
   description = "This is used to toggle which role policy will be used, by default it assumes you are not in government cloud."
 }
+
+variable "region" {
+  default = "us-east-1"
+  description = "aws account region"
+}
